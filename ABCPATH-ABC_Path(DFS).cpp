5aa58e177f9ab1a4014ac917a26232b28feacdd6
jdmoyle <82326361+jdmoyle@users.noreply.github.com>
2021-07-17 21:14:59 +0530

    Update ABCPATH-ABC_Path(DFS).cpp

--- ABCPATH-ABC_Path(DFS).cpp
+++ ABCPATH-ABC_Path(DFS).cpp
@@ -2,85 +2,38 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-
-//int a[8]={0,1,1,-1,-1,0,-1,1};
-//int b[8]={1,0,1,-1,0,-1,1,-1};
-
+ 
+int a[8]={0,1,1,-1,-1,0,-1,1};
+int b[8]={1,0,1,-1,0,-1,1,-1};
+ 
 int n,m;
 //int x,y;
 int dis[51][51];
 char maze[51][51];
-
+ 
 bool isValid(int a,int b)
 {
 	return (a>=0 && b>=0 && a<n && b<m );
 }
-
+ 
 void dfs(int si,int sj,int dist)
 {
 	
 	int x,y;
 	dis[si][sj]=dist+1;
 	
-	x=si;
-	y=sj+1;
-	if(isValid(x,y)&& maze[x][y]==maze[si][sj]+1 && dis[x][y]<dis[si][sj]+1)
+	
+	for(int i=0;i<8;i++)
 	{
+		x=si+a[i];
+		y=sj+b[i];
+		
+		if(isValid(x,y)&& maze[x][y]==maze[si][sj]+1 && dis[x][y]<dis[si][sj]+1)
 		dfs(x,y,dis[si][sj]);
 	}
-	
-	x=si+1;
-	y=sj;
-	if(isValid(x,y)&& maze[x][y]==maze[si][sj]+1 && dis[x][y]<dis[si][sj]+1)
-	{
-		dfs(x,y,dis[si][sj]);
-	}
-	
-	x=si+1;
-	y=sj+1;
-	if(isValid(x,y)&& maze[x][y]==maze[si][sj]+1 && dis[x][y]<dis[si][sj]+1)
-	{
-		dfs(x,y,dis[si][sj]);
-	}
-	
-	x=si-1;
-	y=sj-1;
-	if(isValid(x,y)&& maze[x][y]==maze[si][sj]+1 && dis[x][y]<dis[si][sj]+1)
-	{
-		dfs(x,y,dis[si][sj]);
-	}
-	
-	x=si-1;
-	y=sj;
-	if(isValid(x,y)&& maze[x][y]==maze[si][sj]+1 && dis[x][y]<dis[si][sj]+1)
-	{
-		dfs(x,y,dis[si][sj]);
-	}
-	
-	x=si;
-	y=sj-1;
-	if(isValid(x,y)&& maze[x][y]==maze[si][sj]+1 && dis[x][y]<dis[si][sj]+1)
-	{
-		dfs(x,y,dis[si][sj]);
-	}
-	
-	x=si-1;
-	y=sj+1;
-	if(isValid(x,y)&& maze[x][y]==maze[si][sj]+1 && dis[x][y]<dis[si][sj]+1)
-	{
-		dfs(x,y,dis[si][sj]);
-	}
-	
-	x=si+1;
-	y=sj-1;
-	if(isValid(x,y)&& maze[x][y]==maze[si][sj]+1 && dis[x][y]<dis[si][sj]+1)
-	{
-		dfs(x,y,dis[si][sj]);
-	}
-	
 }
-
-
+ 
+ 
 int main() {
 	
 	int t=0;
@@ -127,4 +80,4 @@ int main() {
 	}
 	
 	return 0;
-}
+} 
