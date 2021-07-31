f23a3a120fc964473de98b7168e8d87425a56817
jdmoyle <82326361+jdmoyle@users.noreply.github.com>
2021-07-31 11:51:46 +0530

    Create CLFLARR - COLORFUL ARRAY.cpp

---
+++ CLFLARR - COLORFUL ARRAY.cpp
@@ -0,0 +1,30 @@
+//O(n);
+
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+int main() {
+	int n,m;
+	cin>>n>>m;
+	int ans[n+1];
+	int left[m],right[m],color[m];
+	memset(ans,0,sizeof(ans));
+	for(int i=0;i<m;i++){
+		int l,r,c;
+		cin>>l>>r>>c;
+		left[i]=l,right[i]=r,color[i]=c;
+	}
+	//coloring from the last query;
+	for(int i=1;i<=n;i++){
+		for(int j=m-1;j>=0;j--){
+			if(i>=left[j] && i<=right[j]){
+				ans[i]=color[j];
+				break;
+			}
+		}
+	}
+	for(int i=1;i<=n;i++)
+	cout<<ans[i]<<endl;
+	return 0;
+}
